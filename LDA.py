7231d1b979f70f068625a87a73299a999af1ab4a
Daniel Wang <dwang@ncsl-dbd-ubuntu.hwcampus.ad.jhu.edu>
2023-07-18 18:50:33 -0400

    add __filter_channels()

--- LDA.py
+++ LDA.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.model_selection import RepeatedKFold
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from scipy.stats import ttest_1samp, ttest_ind
+from scipy.stats import ttest_1samp
 
 class LDA(object):
     def __init__(
-        self
+        self,
+        setup_data = None
         ):
         self.mean_scores = []
         self.std_scores = []
@@ -17,9 +18,36 @@ class LDA(object):
         self.diff_avg_powers = []
         self.lda_coefs = []
 
+        if not setup_data == None:
+            self.__elec_areas = setup_data['elec_area']
+
+    @property
+    def num_trials(self):
+        return self.__num_trials
+
+    @property
+    def num_channels(self):
+        return self.__num_channels
+
+    @property
+    def num_freqs(self):
+        return self.__num_freqs
+    
+    @property
+    def num_timesteps(self):
+        return self.__num_timesteps
+    
+    @property
+    def time_resolution(self):
+        return self.__time_resolution
+    
+    @property
+    def rescaled_timesteps(self):
+        return self.__rescaled_timesteps
+
     def __create_X(self, data, channel, time):
         if type(time) == int:
-            X = data[:, channel, :, time:time+self._time_resolution].mean(-1)
+            X = data[:, channel, :, time:time+self.__time_resolution].mean(-1)
         else:
             X = data[:, channel, :, time]
 
@@ -31,17 +59,19 @@ class LDA(object):
                 setattr(self, attr_name, np.reshape(getattr(self, attr_name), new_shape))
 
     def __set_attributes(self, data, time_resolution):
-        self._num_trials, self._num_channels, self._num_freqs, self._num_timesteps = data.shape
+        self.__num_trials, self.__num_channels, self.__num_freqs, self.__num_timesteps = data.shape
 
         # Checks that input value for time resolution is valid
         if time_resolution == None:
             pass
-        elif not(self._num_timesteps % time_resolution == 0):
+        elif not(self.__num_timesteps % time_resolution == 0):
             raise Exception("Invalid time resolution size, num_timesteps % resolution must equal 0")
         else:
-            self._time_resolution = time_resolution
-            self._rescaled_timesteps = int(self._num_timesteps/time_resolution)
+            self.__time_resolution = time_resolution
+            self.__rescaled_timesteps = int(self.__num_timesteps/time_resolution)
 
+    def __filter_channels(self):
+        return [i for i,ea in enumerate(self.__elec_areas) if ea not in ['white matter','CZ','PZ', 'out','FZ','cerebrospinal fluid','lesion L','ventricle L','ventricle R']]
 
     def __train(self, X, y):
         low_bet_avg_powers = X[np.where(y == 0)].mean(0)
@@ -56,7 +86,7 @@ class LDA(object):
 
         estimators = []
         scores = []
-        dval = np.zeros(self._num_trials)
+        dval = np.zeros(self.__num_trials)
 
         for train, test in rkf.split(X):
             lda = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
@@ -78,29 +108,33 @@ class LDA(object):
     def train_per_channel_and_timestep(self, data, y, time_resolution):
         self.__set_attributes(data, time_resolution)
 
-        for channel in range(self._num_channels):
-            for time in range(self._rescaled_timesteps):
-                X = self.__create_X(data, channel, time*self._time_resolution)
+        for channel in range(self.__num_channels):
+            for time in range(self.__rescaled_timesteps):
+                X = self.__create_X(data, channel, time*self.__time_resolution)
                 self.__train(X, y)
 
-        self.__reshape_attributes((self._num_channels,self._rescaled_timesteps,-1))
+        self.__reshape_attributes((self.__num_channels,self.__rescaled_timesteps,-1))
 
-    def train_on_all_channels(self, data, y, time_resolution):
+    def train_on_all_channels(self, data, y, time_resolution, filter_channels:bool = False):
         self.__set_attributes(data, time_resolution)
 
-        for time in range(self._rescaled_timesteps):
-            X = self.__create_X(data, np.arange(self._num_channels), time*self._time_resolution)
-            X_reshaped = X.reshape(self._num_trials,-1)
+        for time in range(self.__rescaled_timesteps):
+            if filter_channels:
+                X = self.__create_X(data, self.__filter_channels(), time*self.__time_resolution)
+            else:
+                X = self.__create_X(data, np.arange(0,self.__num_channels), time*self.__time_resolution)
+
+            X_reshaped = X.reshape(self.__num_trials,-1)
             self.__train(X_reshaped,y)
         
-        self.__reshape_attributes((self._rescaled_timesteps,-1))
+        self.__reshape_attributes((self.__rescaled_timesteps,-1))
     
     def train_on_all_timesteps(self, data, y):
         self.__set_attributes(data, None)
 
-        for channel in range(self._num_channels):
-            X = self.__create_X(data, channel, np.arange(self._num_timesteps))
-            X_reshaped = X.reshape(self._num_trials, -1)
+        for channel in range(self.__num_channels):
+            X = self.__create_X(data, channel, np.arange(self.__num_timesteps))
+            X_reshaped = X.reshape(self.__num_trials, -1)
             self.__train(X_reshaped, y)
         
-        self.__reshape_attributes((self._num_channels,-1))
+        self.__reshape_attributes((self.__num_channels,-1))
