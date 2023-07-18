024386176fb18107c1e881ccd6daf791d454e976
Daniel Wang <dwang@ncsl-dbd-ubuntu.hwcampus.ad.jhu.edu>
2023-07-18 18:46:46 -0400

    add PerTimestepAllChannels class

--- PlotLDAMetrics.py
+++ PlotLDAMetrics.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-class PlotLDAMetrics(object):
+class PerChannelTimestep(object):
 
     def __init__(
         self,
@@ -18,46 +18,46 @@ class PlotLDAMetrics(object):
 
     def __sort_scores(self, filter_channels:bool):
 
-        mean_scores_max = np.zeros((self.__estimator._num_channels,3))
+        max_mean_scores = np.zeros((self.__estimator.num_channels,3))
 
-        for channel in range(self.__estimator._num_channels):
-            mean_scores_max[channel, 0] = int(channel) # store the channel index
-            mean_scores_max[channel, 1] = list(self.__estimator.mean_scores[channel]).index(max(self.__estimator.mean_scores[channel])) # the time point at which the maximum mean score occurs
-            mean_scores_max[channel, 2] = max(self.__estimator.mean_scores[channel]) # value of the maximum mean score in a particular channel for all time points
+        for channel in range(self.__estimator.num_channels):
+            max_mean_scores[channel, 0] = int(channel) # store the channel index
+            max_mean_scores[channel, 1] = list(self.__estimator.mean_scores[channel]).index(max(self.__estimator.mean_scores[channel])) # the time point at which the maximum mean score occurs
+            max_mean_scores[channel, 2] = max(self.__estimator.mean_scores[channel]) # value of the maximum mean score in a particular channel for all time points
 
         if filter_channels == True:
             elec_areas_filtered_idx = [i for i,ea in enumerate(self.__elec_areas) if ea not in ['white matter','CZ','PZ', 'out','FZ','cerebrospinal fluid','lesion L','ventricle L','ventricle R']]
-            sorted_indices = mean_scores_max[elec_areas_filtered_idx,2].argsort()[::-1]
-            self.mean_scores_max_sorted = mean_scores_max[elec_areas_filtered_idx][sorted_indices]
-            self.elec_names_sorted = self.__elec_names[np.int_(self.mean_scores_max_sorted[:, 0])]
-            self.elec_areas_sorted = self.__elec_areas[np.int_(self.mean_scores_max_sorted[:, 0])]
+            sorted_indices = max_mean_scores[elec_areas_filtered_idx,2].argsort()[::-1]
+            self.sorted_max_mean_scores = max_mean_scores[elec_areas_filtered_idx][sorted_indices]
+            self.sorted_elec_names = self.__elec_names[np.int_(self.sorted_max_mean_scores[:, 0])]
+            self.sorted_elec_areas = self.__elec_areas[np.int_(self.sorted_max_mean_scores[:, 0])]
             
         else:
-            sorted_indices = mean_scores_max[:,2].argsort()[::-1]
-            self.mean_scores_max_sorted = mean_scores_max[sorted_indices]
-            self.elec_names_sorted = self.__elec_names[sorted_indices]
-            self.elec_areas_sorted = self.__elec_areas[sorted_indices]
+            sorted_indices = max_mean_scores[:,2].argsort()[::-1]
+            self.sorted_max_mean_scores = max_mean_scores[sorted_indices]
+            self.sorted_elec_names = self.__elec_names[sorted_indices]
+            self.sorted_elec_areas = self.__elec_areas[sorted_indices]
 
     def plot_sorted_scores(self, out_path:str):
-        num_channels = len(self.mean_scores_max_sorted)
+        num_channels = len(self.sorted_max_mean_scores)
         
         fig, axs = plt.subplots(3, 1, figsize=(24,24), gridspec_kw={'height_ratios' : [1,1,1]})
 
         axs[0].set_title('Sorted Peak Score of LDA Models (from greatest to least)')
         axs[0].set_ylabel('Peak Mean Score')
         axs[0].set_xlabel('Channels (from most to least accurate)')
-        axs[0].plot(np.arange(0,num_channels,1), self.mean_scores_max_sorted[:,2])
+        axs[0].plot(np.arange(0,num_channels,1), self.sorted_max_mean_scores[:,2])
 
         axs[1].set_title('Scatter Plot of Peak Score of LDA Models (from greatest to least) with error bars')
         axs[1].set_ylabel('Peak Mean Score')
         axs[1].set_xlabel('Channels (from most to least accurate)')
-        axs[1].scatter(np.arange(0,num_channels,1), self.mean_scores_max_sorted[:,2])
-        axs[1].errorbar(np.arange(0,num_channels,1), self.mean_scores_max_sorted[:,2], yerr=self.__estimator.std_scores[np.int_(self.mean_scores_max_sorted[:,0]),np.int_(self.mean_scores_max_sorted[:,1])].flatten(), fmt="o")
+        axs[1].scatter(np.arange(0,num_channels,1), self.sorted_max_mean_scores[:,2])
+        axs[1].errorbar(np.arange(0,num_channels,1), self.sorted_max_mean_scores[:,2], yerr=self.__estimator.std_scores[np.int_(self.sorted_max_mean_scores[:,0]),np.int_(self.sorted_max_mean_scores[:,1])].flatten(), fmt="o")
         
         axs[2].set_title('Time of Peak Score of LDA Models')
         axs[2].set_ylabel('Time (seconds)')
         axs[2].set_xlabel('Channels (from most to least accurate)')
-        time = self.mean_scores_max_sorted[:,1]/(20/self.__estimator._time_resolution) - 3
+        time = self.sorted_max_mean_scores[:,1]/(20/self.__estimator.time_resolution) - 3
         axs[2].scatter(np.arange(0, num_channels), time)
         axs[2].axhline(y = 0, color = 'red', alpha=0.5)
 
@@ -68,26 +68,26 @@ class PlotLDAMetrics(object):
         plt.title('Sorted Peak Score of LDA Models (from greatest to least)')
         plt.ylabel('Channel Names')
         plt.xlabel('Score')
-        plt.hlines(y=np.arange(0,num_channels), xmin=0.5, xmax=self.mean_scores_max_sorted[:,2][::-1],
+        plt.hlines(y=np.arange(0,num_channels), xmin=0.5, xmax=self.sorted_max_mean_scores[:,2][::-1],
             color='blue', alpha=0.6, linewidth=2)
-        plt.yticks(np.arange(0,num_channels), labels=self.elec_areas_sorted[::-1])
+        plt.yticks(np.arange(0,num_channels), labels=self.sorted_elec_areas[::-1])
 
         plt.savefig(out_path + f'_sorted_scores_hline')
     
     def plot_sorted_scores_per_channel(self, num_plots:int, out_path:str):
-        time_resolution = self.__estimator._time_resolution
-        rescaled_timesteps = self.__estimator._rescaled_timesteps
+        time_resolution = self.__estimator.time_resolution
+        rescaled_timesteps = self.__estimator.rescaled_timesteps
         
         times = (np.arange(0, rescaled_timesteps, 1) / (20/time_resolution)) - 3 # time 0 seconds denotes when the subject starts moving (i.e. 3 seconds into the data)
 
         fig, axs = plt.subplots(num_plots, 1, figsize=(24, 8 * num_plots))
 
-        for i, trial_data in enumerate(self.mean_scores_max_sorted[:num_plots]):
+        for i, trial_data in enumerate(self.sorted_max_mean_scores[:num_plots]):
             channel, time, peak_accuracy = trial_data
             time = time/(20/time_resolution) - 3
             ax = axs[i]
             ax.plot(times[:], self.__estimator.mean_scores[int(channel)])
-            ax.set_title('Electrode %s in the %s' %(self.elec_names_sorted[i], self.elec_areas_sorted[i]))
+            ax.set_title('Electrode %s in the %s' %(self.sorted_elec_names[i], self.sorted_elec_areas[i]))
             ax.set_ylabel('Score')
             ax.set_xlabel('Time (seconds)')
             # ax.tick_params(axis='both', labelsize=12)
@@ -99,9 +99,9 @@ class PlotLDAMetrics(object):
         plt.show()
 
     def plot_power_heatmap(self, plot_metric, num_plots:int, out_path:str):
-        num_freqs = self.__estimator._num_freqs
-        time_resolution = self.__estimator._time_resolution
-        rescaled_timesteps = self.__estimator._rescaled_timesteps
+        num_freqs = self.__estimator.num_freqs
+        time_resolution = self.__estimator.time_resolution
+        rescaled_timesteps = self.__estimator.rescaled_timesteps
 
         if rescaled_timesteps >= 50:
             num_xticks = int(rescaled_timesteps/2)
@@ -120,7 +120,7 @@ class PlotLDAMetrics(object):
             yticks = np.arange(num_freqs, step=2)
             yticklabels = [round(i,1) for i in np.logspace(np.log2(2),np.log2(150), num = len(yticks),base=2, dtype=np.float16)]
 
-        for i, trial_data in enumerate(self.mean_scores_max_sorted[:num_plots]):
+        for i, trial_data in enumerate(self.sorted_max_mean_scores[:num_plots]):
             channel, time, peak_accuracy = trial_data
 
             low_bet_powers = self.__estimator.low_bet_avg_powers[int(channel)]
@@ -132,24 +132,14 @@ class PlotLDAMetrics(object):
             
             # Plot power per frequency as a function of time, power averaged across all respective trials (high or low bet trials) 
             sns.heatmap(high_bet_powers.T, ax=axs[0][0], vmin=-.4, vmax=.4, cbar_kws={"label": "Z-Scored Frequency Power"}, cmap='PRGn')
-            axs[0][0].set_title('Electrode %s in the %s \n High Bet Z-Scored Frequency Power (n = %s)' %(self.elec_names_sorted[i], self.elec_areas_sorted[i], "~"))
+            axs[0][0].set_title('Electrode %s in the %s \n High Bet Z-Scored Frequency Power (n = %s)' %(self.sorted_elec_names[i], self.sorted_elec_areas[i], "~"))
             axs[0][0].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
-            axs[0][0].set_xticks(xticks, labels = xticklabels)
+            # axs[0][0].set_xticks(xticks, labels = xticklabels)
 
             sns.heatmap(low_bet_powers.T, ax=axs[0][1], vmin=-.4, vmax=.4, cbar_kws={"label": "Z-Scored Frequency Power"}, cmap='PRGn')
-            axs[0][1].set_title('Electrode %s in the %s \n Low Bet Z-Scored Frequency Power (n = %s)' %(self.elec_names_sorted[i], self.elec_areas_sorted[i], "~"))
+            axs[0][1].set_title('Electrode %s in the %s \n Low Bet Z-Scored Frequency Power (n = %s)' %(self.sorted_elec_names[i], self.sorted_elec_areas[i], "~"))
             axs[0][1].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
-            axs[0][1].set_xticks(xticks, labels = xticklabels)
-
-            # ax = sns.heatmap(diff_bet_powers.T, vmin=-.4, vmax=.4, cbar_kws={"label": "Frequency Power", "pad": 0.1}, cmap='PRGn')
-            # ax.set_title('Electrode %s in the %s \n Difference in Frequency Power (High - Low Bet)' %(elec_names[i], elec_areas[i]))
-            # ax.set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
-            # ax.set_xticks(xticks, labels = xticklabels)
-            # ax.axvline(time, color = 'red', alpha=0.5)
-            # ax.axvline(0, color = 'blue', alpha=0.25, ls = '--')
-            # ax_ = ax.twinx()
-            # sns.lineplot(x=np.arange(0,rescaled_timesteps), y=plot_metric[int(channel)], color='blue', ax=ax_) # Make the overlayed metric an optional variable user can select
-            # ax_.set_ylabel('Mean LDA Score')
+            # axs[0][1].set_xticks(xticks, labels = xticklabels)
 
             # Plot power per frequency at a particular timestep for each respective trial (high or low bet trials)
             # sns.heatmap(low_bet_powers[int(time)].T, ax=axs[0][0], vmin=-3, vmax=3, cbar_kws={"label": "Frequency Power"}, cmap='PRGn')
@@ -162,9 +152,9 @@ class PlotLDAMetrics(object):
 
             # Plots the difference in power frequency for high and low bet trials
             sns.heatmap(diff_bet_powers.T, ax=axs[1][0], vmin=-.4, vmax=.4, cbar_kws={"label": "Z-Scored Frequency Power", "pad": 0.1}, cmap='PRGn')
-            axs[1][0].set_title('Electrode %s in the %s \n Difference in Z-Scored Frequency Power (High - Low Bet)' %(self.elec_names_sorted[i], self.elec_areas_sorted[i]))
+            axs[1][0].set_title('Electrode %s in the %s \n Difference in Z-Scored Frequency Power (High - Low Bet)' %(self.sorted_elec_names[i], self.sorted_elec_areas[i]))
             axs[1][0].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
-            axs[1][0].set_xticks(xticks, labels = xticklabels)
+            # axs[1][0].set_xticks(xticks, labels = xticklabels)
             ax = axs[1][0].twinx()
             sns.lineplot(x=np.arange(0,rescaled_timesteps), y=plot_metric[int(channel)].flatten(), color='blue', ax=ax) # Make the overlayed metric an optional variable user can select
             ax.set_ylabel('Mean LDA Score')
@@ -173,14 +163,13 @@ class PlotLDAMetrics(object):
 
             # Plots the LDA coefficients for each frequency band over time
             sns.heatmap(lda_coef.T, ax=axs[1][1], vmin=-1, vmax=1, cbar_kws={"label": "Z-Scored Frequency Power"}, cmap='PRGn')
-            axs[1][1].set_title('LDA coefficient values for all frequencies \n at %s in %s' %(self.elec_names_sorted[i], self.elec_areas_sorted[i]))
+            axs[1][1].set_title('LDA coefficient values for all frequencies \n at %s in %s' %(self.sorted_elec_names[i], self.sorted_elec_areas[i]))
             axs[1][1].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
-            axs[1][1].set_xticks(xticks, labels = xticklabels)
+            # axs[1][1].set_xticks(xticks, labels = xticklabels)
 
             for axs_ in axs:
                 for ax in axs_:
-                    # ax.set_xticks(xticks)
-                    # ax.set_xticklabels(xticklabels)
+                    ax.set_xticks(xticks, labels = xticklabels)
                     ax.set_yticks(yticks)
                     ax.set_yticklabels(yticklabels, rotation = 0)
                     ax.axes.invert_yaxis()
@@ -190,5 +179,120 @@ class PlotLDAMetrics(object):
                     for label in ax.xaxis.get_ticklabels()[1::2]:
                         label.set_visible(False)
 
-            plt.savefig(out_path + '_heatmap_%s_%s'%(self.elec_names_sorted[i], self.elec_areas_sorted[i]))
+            plt.savefig(out_path + '_heatmap_%s_%s'%(self.sorted_elec_names[i], self.sorted_elec_areas[i]))
             plt.show()
+
+class PerTimestepAllChannels(object):
+
+    def __init__(
+        self,
+        estimator
+        ) :
+        self.__estimator = estimator
+
+        self.__sort_scores()
+        self.__convert_timesteps_to_time(3)
+
+    def __sort_scores(self):
+
+        enumerated_mean_scores = np.array(list(enumerate(self.__estimator.mean_scores.flatten())))
+        sorted_indices = enumerated_mean_scores[:,1].argsort()[::-1]
+        self.sorted_mean_scores = enumerated_mean_scores[sorted_indices]
+
+    def __convert_timesteps_to_time(self, event_delay):
+        self.__event_delay = event_delay
+        self.__times = (np.arange(0, self.__estimator.rescaled_timesteps) / (20/self.__estimator.time_resolution)) - event_delay # time 0 seconds denotes when the subject starts moving (i.e. 3 seconds into the data)
+
+    def plot_sorted_scores(self, out_path:str):
+        num_timesteps = self.__estimator.rescaled_timesteps
+        xticks = np.arange(0,num_timesteps,1)
+        
+        fig, axs = plt.subplots(3, 1, figsize=(24,24), gridspec_kw={'height_ratios' : [1,1,1]})
+
+        axs[0].set_title('Sorted Peak Score of LDA Models (from greatest to least)')
+        axs[0].set_ylabel('Mean Score')
+        axs[0].set_xlabel('Times (from most to least accurate)')
+        axs[0].set_xticks(xticks, labels = self.__times[np.int_(self.sorted_mean_scores[:,0])])
+        axs[0].plot(xticks ,self.sorted_mean_scores[:,1])
+
+        axs[1].set_title('Scatter Plot of Peak Score of LDA Models (from greatest to least) with error bars')
+        axs[1].set_ylabel('Mean Score')
+        axs[1].set_xlabel('Times (from most to least accurate)')
+        axs[1].set_xticks(xticks, labels = self.__times[np.int_(self.sorted_mean_scores[:,0])])
+        axs[1].scatter(xticks, self.sorted_mean_scores[:,1])
+        axs[1].errorbar(xticks, self.sorted_mean_scores[:,1], yerr=self.__estimator.std_scores[np.int_(self.sorted_mean_scores[:,0])].flatten(), fmt="o")
+
+        axs[2].set_title('Sorted Peak Score of LDA Models (from greatest to least)')
+        axs[2].set_ylabel('Times (from most to least accurate)')
+        axs[2].set_xlabel('Score')
+        axs[2].hlines(y=np.arange(0,num_timesteps), xmin=0.5, xmax=self.sorted_mean_scores[:,1][::-1],
+            color='blue', alpha=0.6, linewidth=2)
+        axs[2].set_yticks(np.arange(0,num_timesteps), labels=self.__times[np.int_(self.sorted_mean_scores[:,0])][::-1])
+
+        fig.tight_layout()
+        fig.savefig(out_path + '_sorted_scores_per_timestep_all_channels')
+    
+    def plot_power_heatmap(self, out_path:str):
+        num_freqs = self.__estimator.num_freqs
+        time_resolution = self.__estimator.time_resolution
+        rescaled_timesteps = self.__estimator.rescaled_timesteps
+
+        low_bet_powers = self.__estimator.low_bet_avg_powers
+        high_bet_powers = self.__estimator.high_bet_avg_powers
+        diff_bet_powers = self.__estimator.diff_avg_powers
+        lda_coef = self.__estimator.lda_coefs
+
+        if rescaled_timesteps >= 50:
+            num_xticks = int(rescaled_timesteps/2)
+        else:
+            num_xticks = rescaled_timesteps
+
+        xticks = np.linspace(0, rescaled_timesteps - 1, num=num_xticks, dtype=np.int_)
+        xticklabels = np.around(np.linspace(0, rescaled_timesteps - 1, num=num_xticks, dtype=np.int_)/(20/time_resolution) - self.__event_delay, decimals=2)
+
+        if num_freqs == 5: 
+            # sets the y-ticks when using EEG frequency bands
+            yticks = np.arange(diff_bet_powers.shape[1], step=5)
+        else:
+            # sets the y-ticks when using all frequencies in data
+            yticks = np.arange(diff_bet_powers.shape[1], step=63)
+            # yticklabels = [round(i,1) for i in np.logspace(np.log2(2),np.log2(150), num = len(yticks),base=2, dtype=np.float16)]
+
+        fig, axs = plt.subplots(2, 2,figsize=(24, 20))
+        
+        # Plot power per frequency as a function of time, power averaged across all respective trials (high or low bet trials) 
+        sns.heatmap(high_bet_powers.T, ax=axs[0][0], vmin=-.4, vmax=.4, cbar_kws={"label": "Z-Scored Frequency Power"}, cmap='PRGn')
+        axs[0][0].set_title('High Bet Z-Scored Frequency Power (n = %s)' %("~"))
+        axs[0][0].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
+
+        sns.heatmap(low_bet_powers.T, ax=axs[0][1], vmin=-.4, vmax=.4, cbar_kws={"label": "Z-Scored Frequency Power"}, cmap='PRGn')
+        axs[0][1].set_title('Low Bet Z-Scored Frequency Power (n = %s)' %("~"))
+        axs[0][1].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
+
+        # Plots the difference in power frequency for high and low bet trials
+        sns.heatmap(diff_bet_powers.T, ax=axs[1][0], vmin=-.4, vmax=.4, cbar_kws={"label": "Z-Scored Frequency Power", "pad": 0.1}, cmap='PRGn')
+        axs[1][0].set_title('Difference in Z-Scored Frequency Power (High - Low Bet)')
+        axs[1][0].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
+        ax = axs[1][0].twinx()
+        sns.lineplot(x=np.arange(0,rescaled_timesteps), y=self.__estimator.mean_scores.flatten(), color='blue', ax=ax) # Make the overlayed metric an optional variable user can select
+        ax.set_ylabel('Mean LDA Score')
+
+        # Plots the LDA coefficients for each frequency band over time
+        sns.heatmap(lda_coef.T, ax=axs[1][1], vmin=-1, vmax=1, cbar_kws={"label": "Z-Scored Frequency Power"}, cmap='PRGn')
+        axs[1][1].set_title('LDA coefficient values for all frequencies')
+        axs[1][1].set(xlabel="Time (sec)", ylabel="Frequency (Hz)")
+
+        for axs_ in axs:
+            for ax in axs_:
+                ax.set_xticks(xticks, labels = xticklabels, rotation = 90)
+                ax.set_yticks(yticks)
+        #        ax.set_yticklabels(yticklabels, rotation = 0)
+                ax.axes.invert_yaxis()
+                ax.axvline(self.sorted_mean_scores[0,0], color = 'red', alpha=0.5)
+                ax.axvline(12, color = 'blue', alpha=1, ls = '--')
+
+                for label in ax.xaxis.get_ticklabels()[1::2]:
+                    label.set_visible(False)
+
+        plt.savefig(out_path + '_heatmap_per_timestep_all_channels')
+        plt.show()
