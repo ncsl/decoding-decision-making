398f754ddf7c4bba5640533b20612093c9879da6
Daniel Wang <dwang@ncsl-dbd-ubuntu.hwcampus.ad.jhu.edu>
2024-06-20 21:39:41 -0400

    run pipeline on show card as event

--- Decoding/GLMTraining.py
+++ Decoding/GLMTraining.py
@@ -11,10 +11,14 @@ from helper_functions import plot_freq_band_data_for_channel
 # %%
 # subs = ['06']
 # subs = ['06','07','10','12','13','15','16','17','18','21']
-subs = ['06', '13', '16', '17', '18', '21']
-# subs = ['07', '10', '12', '15', '16']
+# subs = ['06', '13', '16', '17', '18', '21'] # left MTG patients
+subs = ['07', '10', '12', '15', '16'] # right MTG patients
 
 file_paths = {}
+
+brain_region = 'RightMiddleTemporalGyrus'
+prediction_variable = 'PredictBet'
+event = 'ShowCard'
 
 for sub in subs:
     # Create a dictionary holding the file paths
@@ -22,10 +26,10 @@ for sub in subs:
     file_paths[sub] = {
         'setup_path': ncsl_share + f'/Public/EFRI/1_formatted/SUBJECT{sub}/EFRI{sub}_WAR_SES1_Setup.mat',
         'raw_path': ncsl_share + f'/Public/EFRI/1_formatted/SUBJECT{sub}/EFRI{sub}_WAR_SES1_Raw.mat',
-        'data_path': ncsl_share + f'/Daniel/Data/Trial_by_Chan_by_Freq_by_Time_Snapshots/Subject{sub}_snapshot_normalized.npy', # movement onset as event,
-        # 'data_path': ncsl_share + f'/Daniel/Data/Trial_by_Chan_by_Freq_by_Time_Snapshots/show-card_pre-2sec_post-4sec/Subject{sub}_snapshot_normalized.npy', # show card as event
-        'out_path_metrics': f'Metrics/LeftMiddleTemporalGyrus/PredictCard/RandomForestShowCard/Subject{sub}',
-        'out_path_plots': f'Plots/LeftMiddleTemporalGyrus/PredictCard/RandomForestShowCard/Subject{sub}'
+        # 'data_path': ncsl_share + f'/Daniel/Data/Trial_by_Chan_by_Freq_by_Time_Snapshots/Subject{sub}_snapshot_normalized.npy', # movement onset as event,
+        'data_path': ncsl_share + f'/Daniel/Data/Trial_by_Chan_by_Freq_by_Time_Snapshots/show-card_pre-2sec_post-4sec/Subject{sub}_snapshot_normalized.npy', # show card as event
+        'out_path_metrics': f'Metrics/{brain_region}/{prediction_variable}/RandomForest{event}/Subject{sub}',
+        'out_path_plots': f'Plots/{brain_region}/{prediction_variable}/RandomForest{event}/Subject{sub}'
     }
 ## %%
 
@@ -69,13 +73,13 @@ for sub in subs:
     #     else:
     #         card_categories.append(1)
 
-    brain_area = 'middle temporal gyrus L'
+    brain_area = 'middle temporal gyrus R'
     channel_idxs = np.where(elec_areas == brain_area)[0]
 
-    # data = np.load(file_paths[sub]['data_path'])[:,channel_idxs,:,40:80] # 40:80 is the time window of interest for visual cue as event
+    data = np.load(file_paths[sub]['data_path'])[:,channel_idxs,:,40:100] # 40:100 is the time window of interest for visual cue as event
     # card_categories = np.asarray(card_categories)
 
-    data = np.load(file_paths[sub]['data_path'])[:,channel_idxs,:,:60] # 0:60 is the time window of interest for movement onset as event
+    # data = np.load(file_paths[sub]['data_path'])[:,channel_idxs,:,:60] # 0:60 is the time window of interest for movement onset as event
     y = np.asarray([(0 if bet == 5 else 1) for bet in bets]) # 0 = low bet ($5), 1 = high bet ($20)
 
     # Group frequencies into frequency bands
@@ -126,7 +130,7 @@ for sub in subs:
     info = zip(ch_names, optimal_time_windows_start, optimal_time_windows_end, accuracies)
 
     brain_area = brain_area.replace(' ', '_')
-    file_name = f'Metrics/RightMiddleTemporalGyrus/PredictBet/RandomForestMovementOnset/ChannelInfo/Subject{sub}_{brain_area}_info.csv'
+    file_name = f'Metrics/{brain_region}/{prediction_variable}/RandomForest{event}/ChannelInfo/Subject{sub}_{brain_area}_info.csv'
 
     with open(file_name, 'w', newline='') as f:
         writer = csv.writer(f)
@@ -138,19 +142,19 @@ for sub in subs:
     #     time_window = optimal_time_windows_info[1]
 
     #     # Plot the data for the optimal time window for each channel
-    #     plot_freq_band_data_for_channel(f_band_data, y, ch, time_window, event_delay=3, out_path_plots=out_path_plots, elec_names=elec_names, elec_areas=elec_areas)
+    #     plot_freq_band_data_for_channel(f_band_data, y, ch, time_window, event_delay=0, out_path_plots=out_path_plots, elec_names=elec_names, elec_areas=elec_areas)
 
     # accuracies, peak_accuracy_group_idx = clf.get_group_accuracies(y)
     # top_chs = [ch for ch, _, _ in clf._optimal_time_windows_per_channel[:peak_accuracy_group_idx+1]]
     # clf.plot_accuracies(y, out_path_plots)
-    # clf.plot_heatmap(top_chs, 0, accuracies[peak_accuracy_group_idx], False, out_path_plots)
+    # clf.plot_heatmap(top_chs, event_delay=0, sub=sub, top_accuracy=accuracies[peak_accuracy_group_idx], optimal_combination=False, out_path=out_path_plots)
     # optimal_time_window_channel_combination = elec_areas[top_chs]
     # np.save(out_path_metrics + '_top_n_channel_combination.npy', optimal_time_window_channel_combination)
 
-    # peak_accuracy, optimal_channel_combination = clf.get_optimal_channel_combination(y, n_channels=num_channels)
-    # # clf.plot_freq_box_plots(y, results[0][0], out_path_plots)
-    # clf.plot_heatmap(optimal_channel_combination, 0, sub, peak_accuracy, True, out_path_plots)
-    # np.save(out_path_metrics + '_optimal_channel_combination.npy', elec_areas[optimal_channel_combination])
+    peak_accuracy, optimal_channel_combination = clf.get_optimal_channel_combination(y, n_channels=num_channels)
+    # clf.plot_freq_box_plots(y, results[0][0], out_path_plots)
+    clf.plot_heatmap(optimal_channel_combination, event_delay=0, sub=sub, top_accuracy=peak_accuracy, optimal_combination=True, out_path=out_path_plots)
+    np.save(out_path_metrics + '_optimal_channel_combination.npy', elec_areas[optimal_channel_combination])
 
 # %%
 # %%
